93b43a16ec6fb378ff62a3b73d9edbee8b73e1d0
Dipti <diptisoni9929@gmail.com>
2023-06-27 13:38:49 +0530

    First Commit

---
+++ src/pages/Login.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react"
+import './login.css';
+
+function LoginPage() {
+
+  let [userName, setUserName] = useState('');
+  let [password, setPassword] = useState('');
+
+  const handleSubmit = () => {
+    if (!userName) alert('User name required')
+    else if (!password) alert('Password required')
+    else alert('Login successfully')
+  }
+
+  return (
+    <div class="login-container">
+      <h1>Sign in</h1>
+      <p className="sub-head">Sign in and start managing your candidates!</p>
+      <form id="login-form">
+        <div class="form-group">
+          <label for="username"></label>
+          <input type="text" id="username" name="username" placeholder="Login" required onChange={(e) => setUserName(e.target.value)} />
+        </div>
+        <div class="form-group">
+          <label for="password"></label>
+          <input type="password" id="password" name="password" placeholder="Password" required onChange={(e) => setPassword(e.target.value)} />
+        </div>
+        <div class="form-group">
+          <div class="row">
+            <div class="col">
+              <input type="checkbox" id="rememberMe" name="rememberMe" style={{ width: '13px' }} />
+              <p style={{ fontSize: '12px' }}>Remember Me</p>
+            </div>
+            <div class="col">
+              <a className="forget-pass">Forgot password?</a>
+            </div>
+          </div>
+        </div>
+        <div class="form-group">
+          <button type="submit" onClick={handleSubmit}>Login</button>
+        </div>
+      </form >
+    </div >
+  );
+}
+
+export default LoginPage;
